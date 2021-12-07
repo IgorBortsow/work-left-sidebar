c576b9be0ae1e7f5e9737733fbe41f73f4e4e82c
Igor <killerzmssss@hmail.com>
2021-12-07 22:49:07 +0200

    full block

---
+++ README.md
@@ -0,0 +1,4 @@
+#work-left-sidebar
+
+![screen](screen.png)?
+![screen](screen2.png)?
---
+++ build/js/main.js
@@ -0,0 +1,12 @@
+
+// Импортируем другие js-файлы
+let btn = document.querySelector('#btn');
+let sidebar = document.querySelector('.sidebar');
+let searchBtn = document.querySelector('.bx-search');
+
+btn.onclick = function () {
+    sidebar.classList.toggle("active");
+}
+searchBtn.onclick = function () {
+    sidebar.classList.toggle("active");
+}
